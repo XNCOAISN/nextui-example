d2ce8dc1bee9ba2598e275de4d85a10026e98b62
XNCOAISN <end.pgm@gmail.com>
2023-09-04 19:33:03 +0900

    💄 add button variants story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -3,9 +3,6 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 const meta = {
   component: Button,
-  parameters: {
-    layout: "fullscreen",
-  },
   tags: ["autodocs"],
   title: "Button",
 } satisfies Meta<typeof Button>;
@@ -17,5 +14,21 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     children: "Button",
+  },
+};
+
+export const Variants: Story = {
+  render: () => {
+    return (
+      <div className="flex flex-col gap-2 w-[200px]">
+        <Button variant="solid">solid</Button>
+        <Button variant="bordered">bordered</Button>
+        <Button variant="light">light</Button>
+        <Button variant="flat">flat</Button>
+        <Button variant="faded">faded</Button>
+        <Button variant="shadow">shadow</Button>
+        <Button variant="ghost">ghost</Button>
+      </div>
+    );
   },
 };
